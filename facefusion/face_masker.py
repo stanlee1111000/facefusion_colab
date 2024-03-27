c3b33029b2c25159712174943731c8d00df3b5b5
henryruhs <info@henryruhs.com>
2024-03-27 01:00:03 +0100

    Only use inference pool

--- facefusion/face_masker.py
+++ facefusion/face_masker.py
@@ -8,14 +8,11 @@ import numpy
 
 import facefusion.globals
 from facefusion import process_manager
-from facefusion.inference_pool import get_inference_session
+from facefusion.inference_pool import get_inference_session, clear_inference_session
 from facefusion.typing import FaceLandmark68, VisionFrame, Mask, Padding, FaceMaskRegion, ModelSet
 from facefusion.filesystem import resolve_relative_path
 from facefusion.download import conditional_download
 
-FACE_OCCLUDER = None
-FACE_PARSER = None
-THREAD_LOCK : threading.Lock = threading.Lock()
 MODELS : ModelSet =\
 {
 	'face_occluder':
@@ -45,37 +42,27 @@ FACE_MASK_REGIONS : Dict[FaceMaskRegion, int] =\
 
 
 def get_face_occluder() -> Any:
-	global FACE_OCCLUDER
-
-	with THREAD_LOCK:
-		if FACE_OCCLUDER is None:
-			model_path = MODELS.get('face_occluder').get('path')
-			FACE_OCCLUDER = get_inference_session(model_path)
-	return FACE_OCCLUDER
+	while process_manager.is_checking():
+		sleep(0.5)
+	model_path = MODELS.get('face_occluder').get('path')
+	return get_inference_session(model_path)
 
 
 def get_face_parser() -> Any:
-	global FACE_PARSER
-
-	with THREAD_LOCK:
-		while process_manager.is_checking():
-			sleep(0.5)
-		if FACE_PARSER is None:
-			model_path = MODELS.get('face_parser').get('path')
-			FACE_PARSER = get_inference_session(model_path)
-	return FACE_PARSER
+	while process_manager.is_checking():
+		sleep(0.5)
+	model_path = MODELS.get('face_parser').get('path')
+	return get_inference_session(model_path)
 
 
 def clear_face_occluder() -> None:
-	global FACE_OCCLUDER
-
-	FACE_OCCLUDER = None
+	model_path = MODELS.get('face_occluder').get('path')
+	clear_inference_session(model_path)
 
 
 def clear_face_parser() -> None:
-	global FACE_PARSER
-
-	FACE_PARSER = None
+	model_path = MODELS.get('face_parser').get('path')
+	clear_inference_session(model_path)
 
 
 def pre_check() -> bool:
